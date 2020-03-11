5918812c7f7e9d5972c9f970d428bd9e20fed996
Anton Kosykh <kelin2025@yandex.ru>
2020-03-11 22:12:02 +0300

    Giveaway is over tho

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ import "./features";
 import { Layout } from "@/features/layout";
 import { HomePage } from "@/pages/Home";
 import { GamesPage } from "@/pages/Games";
-import { ContestPage } from "@/pages/Contest";
 import { ChallengesPage } from "@/pages/Challenges";
 
 import "@/ui/assets/css/reset.css";
 
-const routes = [HomePage, GamesPage, ChallengesPage, ContestPage];
+const routes = [HomePage, GamesPage, ChallengesPage];
 
 using(document.getElementById("app"), () => {
   Layout(routes);
