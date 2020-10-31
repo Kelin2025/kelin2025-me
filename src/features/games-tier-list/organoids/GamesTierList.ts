228890b185fd0ac20192764b417f80a4aab34a7a
Anton Kosykh <kelin2025@yandex.ru>
2020-10-31 07:06:48 +0300

    Hide the game if no steam info provided

--- src/features/games-tier-list/organoids/GamesTierList.ts
+++ src/features/games-tier-list/organoids/GamesTierList.ts
@@ -1,4 +1,4 @@
-import { h } from "forest";
+import { h, spec } from "forest";
 
 import { $gamesByTiers } from "@/api/games-tier-list";
 
@@ -9,9 +9,13 @@ export const GamesTierList = () => {
   TierList({
     data: $gamesByTiers,
     view: ({ store }) => {
-      const icon = store.map((game) =>
-        getIconPath({ id: game.appid, icon: game.steam.icon })
+      const icon = store.map(
+        (game) =>
+          game.steam && getIconPath({ id: game.appid, icon: game.steam.icon })
       );
+      spec({
+        visible: store.map((game) => !!game.steam),
+      });
       h("img", {
         attr: { src: icon },
         style: { borderRadius: "5px" },
